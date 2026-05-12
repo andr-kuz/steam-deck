7542fbeb0fe74bc2a1e843f6e70183de3333977d
awakum <awakum192@gmail.com>
2026-05-12 13:05:54 +0300

    style: getting rid of temp file

--- install-phase.sh
+++ install-phase.sh
@@ -6,7 +6,7 @@ mkdir -p "$out/share/applications"
 
 cp "@vortexWrapperScript@" "$out/share/applications/vortex-wrapper.sh"
 chmod +x "$out/share/applications/vortex-wrapper.sh"
+
 sed "s|\$out|$out|g" "@desktopItem@" | \
-  sed "s|\$home|@homeDir@|g" > temp
-mv temp "$out/share/applications/vortex.desktop"
+  sed "s|\$home|@homeDir@|g" > "$out/share/applications/vortex.desktop"
 cp "@desktopItemIcon@" "$out/share/applications/vortex.ico"
